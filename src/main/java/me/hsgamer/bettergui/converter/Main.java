75d9ab5ec923efefc13ddc1c9874c8883c9d77b0
HSGamer <huynhqtienvtag@gmail.com>
2020-03-25 20:11:52 +0700

    support directory

--- src/main/java/me/hsgamer/bettergui/converter/Main.java
+++ src/main/java/me/hsgamer/bettergui/converter/Main.java
@@ -4,7 +4,10 @@ import de.leonhard.storage.Config;
 import de.leonhard.storage.LightningBuilder;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
@@ -43,35 +46,54 @@ public class Main {
           logger.info("Created output folder");
         }
 
-        for (String filename : args) {
-          File file = new File(filename);
-          if (file.exists()) {
-            logger.info(() -> "CONVERTING \t--> " + filename);
+        for (File file : getFiles(logger, args)) {
+          String filename = file.getName();
+          logger.info(() -> "CONVERTING \t--> " + filename);
 
-            File output = new File("output", file.getName());
-            if (output.createNewFile()) {
-              logger.info("Created output file");
-            }
-
-            // Init config
-            Config fromConfig = LightningBuilder.fromFile(file).createConfig();
-            Config toConfig = LightningBuilder.fromFile(output).createConfig();
-
-            // Start the convert
-            type.convert(logger, fromConfig, toConfig);
-
-            // Finish
-            logger.info(() -> "CONVERTED \t--> " + outputDir.getName() + File.separator + filename);
-          } else {
-            logger.log(Level.WARNING, "{0} is not found", filename);
+          File output = new File("output", file.getName());
+          if (output.createNewFile()) {
+            logger.info("Created output file");
           }
+
+          // Init config
+          Config fromConfig = LightningBuilder.fromFile(file).createConfig();
+          Config toConfig = LightningBuilder.fromFile(output).createConfig();
+
+          // Start the convert
+          type.convert(logger, fromConfig, toConfig);
+
+          // Finish
+          logger.info(() -> "CONVERTED \t--> " + outputDir.getName() + File.separator + filename);
         }
       } catch (IllegalArgumentException e) {
         logger.warning("Cannot find the converter for " + args[0]);
         logger.warning("Available: " + Arrays.toString(Type.values()));
       }
     } else {
-      logger.info("Usage: java -Dtype=<type> -jar Converter-<version>.jar <filename>.yml");
+      logger.info(
+          "Usage: java -Dtype=<type> -jar Converter-<version>.jar <filename> [<filename1> <filename2> ...]");
     }
+  }
+
+  private static List<File> getFiles(Logger logger, String[] input) {
+    List<File> files = new ArrayList<>();
+    for (String name : input) {
+      files.addAll(getFiles(logger, new File(name)));
+    }
+    return files;
+  }
+
+  private static List<File> getFiles(Logger logger, File file) {
+    List<File> list = new ArrayList<>();
+    if (file.isDirectory()) {
+      for (File subFile : Objects.requireNonNull(file.listFiles())) {
+        list.addAll(getFiles(logger, subFile));
+      }
+    } else if (file.isFile() && file.exists()) {
+      list.add(file);
+    } else {
+      logger.warning("Invalid file: " + file.getName());
+    }
+    return list;
   }
 }
